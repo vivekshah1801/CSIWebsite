f51f99359bf415a25e1dbfa4111026444b2ec9f6
Bhargav Lad <ladbhargav@yahoo.co.in>
2018-01-30 20:44:16 +0530

    contact us added

---
+++ contact_us.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    
+  </head>
+  
+<style>
+#contact .card:hover i,#contact .card:hover h2{
+	color: black;
+}
+.fa{
+  color: #003399;
+}
+</style>
+<body>
+<section id="contact">
+
+       <div class="container">
+           <h2 class="text-center text-uppercase">Contact us</h2>
+           <p class="text-center w-75 m-auto">If you have any queries, please don’t hesitate to contact us.	
+                Please feel free to call us on (phone) or contact by&nbsp;(email), if you require any further information.</p>
+           <div class="row">
+             <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+               <div class="card border-0">
+                  <div class="card-body text-center">
+                    <i class="fa fa-phone fa-5x mb-3" aria-hidden="true"></i>
+                    <h4 class="text-uppercase mb-5">call us</h4>
+                    <p>Prakriti&nbsp;+8805152397,<br/>Abhishek&nbsp;+918866052534</p>
+                  </div>
+                </div>
+             </div>
+             <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+               <div class="card border-0">
+                  <div class="card-body text-center">
+                    <i class="fa fa-facebook-square fa-5x mb-3" aria-hidden="true"></i>
+                    <h4 class="text-uppercase mb-5">Facebook</h4>
+                   <address>Suite 02, Level 12, Sahera Tropical Center </address>
+                  </div>
+                </div>
+             </div>
+             <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+               <div class="card border-0">
+                  <div class="card-body text-center">
+                    <i class="fa fa-instagram fa-5x mb-3" aria-hidden="true"></i>
+                    <h4 class="text-uppercase mb-5">Instagram</h4>
+                    <address>Suite 02, Level 12, Sahera Tropical Center </address>
+                  </div>
+                </div>
+             </div>
+             <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+               <div class="card border-0">
+                  <div class="card-body text-center">
+                    <i class="fa fa-globe fa-5x mb-3" aria-hidden="true"></i>
+                    <h4 class="text-uppercase mb-5">email</h4>
+                    <p>csi.ddit@gmail.com</p>
+                  </div>
+                </div>
+             </div>
+           </div>
+       </div>
+    </section>
+</body>
