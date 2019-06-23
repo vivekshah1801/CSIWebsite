5c1748eff6307ac02c4f72ce76e8322ad709e04f
vivekshah1801 <vivekshah9969@gmail.com>
2019-06-23 20:40:44 +0530

    added team members dummy, added footer code

--- events.php
+++ events.php
@@ -146,55 +146,8 @@
 	  </div>
 	</section>
 
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-9 mt-3">
-            <div class="row">
-              <div class="col-md-5">
-                <h2 class="footer-heading mb-4">About Us</h2>
-                <p>Hello, please write a decent about CSI text here.</p>
-              </div>
-              <div class="col-md-3 ml-auto">
-                <h2 class="footer-heading mb-4">Quick Links</h2>
-                <ul class="list-unstyled">
-				  <li><a href="#">Contact Us</a></li>
-  				  <li><a href="#">Resourses</a></li>
-  				  <li><a href="#">Media Kit</a></li>
-  				  <li><a href="#">Registrations</a></li>
-                </ul>
-              </div>
-              <div class="col-md-3 footer-social">
-                <h2 class="footer-heading mb-4">Follow Us</h2>
-				<ul class="list-unstyled">
-                	<li><a href="#">Facebook</a></li>
-                	<li><a href="#">Instagram</a></li>
-                	<li><a href="#">LinkedIn</a></li>
-					<li><a href="#">Github</a></li>
-				</ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <h2 class="footer-heading mb-4">Contact Us</h2>
-			<p>Vivek   : +91 88492 36413 </p>
-			<p>Trupti : +91 91064 73664 </p>
-          </div>
-        </div>
+	<?php include("footer.php"); ?>
 
-        <div class="row mt-1 pt-1 text-center">
-          <div class="col-md-12">
-            <div class="border-top pt-3">
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              <p> Copyright &copy; 2019 | Designed & Developed by CSI DDU Student Branch </p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </footer>
 
   </div> <!-- .site-wrap -->
 
